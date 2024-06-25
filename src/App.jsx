01fe32d101b7934cee4ed0872bbe618efff2f66c
jayden-jy <markup.jy@gmail.com>
2024-06-25 15:33:19 +0900

    docs: setting

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+function App() {
+  return (
+    <main id="game-container">
+      <ol id="players">
+        <li>
+          <span className="player-name">player 1</span>
+          <span className="player-symbol">X</span>
+        </li>
+        <li>
+          <span className="player-name">player 2</span>
+          <span className="player-symbol">O</span>
+        </li>
+      </ol>
+    </main>
+  )
+}
+
+export default App
