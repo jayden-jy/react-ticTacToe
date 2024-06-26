f507243335ca05e9e501b12e48dd9e6c26792b71
jayden-jy <markup.jy@gmail.com>
2024-06-26 15:16:19 +0900

    docs: 양방향 바인딩

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <main id="game-container">
       <ol id="players">
-        <Player name="player 1" symbol="X" />
-        <Player name="player 2" symbol="O" />
+        <Player initialName="player 1" symbol="X" />
+        <Player initialName="player 2" symbol="O" />
       </ol>
     </main>
   )
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,34 @@
-export default function player({ name, symbol }) {
+import { useState } from "react";
+
+export default function player({ initialName, symbol }) {
+  const [ playerName, setPlayerName ] = useState(initialName)
+  const [ isEditing, setisEditing ] = useState(false);
+
+  function handleEditClick() {
+    //setisEditing(!isEditing); // 해당값이 반전 됨. 이 방법은 여러 번의 상태 업데이트가 일어날 때 정확하게 동작하지 않을 수 있음
+    setisEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    // console.log(event);
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  // let btnCaption = "수정";
+
+  if (isEditing) {
+    editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
+    // btnCaption = "저장";
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{ isEditing ? "저장" : "수정" }</button>
     </li>
   )
 }
