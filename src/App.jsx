c5d0b762abc4cabce393de19e6483a0840a0626b
jayden-jy <markup.jy@gmail.com>
2024-06-26 17:32:30 +0900

    docs: state 업데이트, 끌어올리기

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
+
 import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => curActivePlayer === "X" ? "O" : "X");
+  }
+
   return (
     <main id="game-container">
-      <ol id="players">
-        <Player initialName="player 1" symbol="X" />
-        <Player initialName="player 2" symbol="O" />
+      <ol id="players" className="highlight-player">
+        <Player initialName="player 1" symbol="X" isActive={activePlayer === "X"} />
+        <Player initialName="player 2" symbol="O" isActive={activePlayer === "O"} />
       </ol>
+      <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
     </main>
   )
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function player({ initialName, symbol }) {
+export default function player({ initialName, symbol, isActive }) {
   const [ playerName, setPlayerName ] = useState(initialName)
   const [ isEditing, setisEditing ] = useState(false);
 
@@ -23,7 +23,7 @@ export default function player({ initialName, symbol }) {
   }
 
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
