c417b7ec96d43d060152ba2db123e8e0b6259d07
jayden-jy <markup.jy@gmail.com>
2024-06-27 16:52:03 +0900

    docs: state

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,68 @@ import { useState } from "react";
 
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
+import Log from "./components/Log.jsx";
+
+// 컴포넌트 관련된 상태나 데이터에 접근할 필요가 없는 함수, 컴포넌트 함수가 재실행될 때 스스로 재실행되지않음
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+
+  return currentPlayer;
+}
 
 function App() {
-  const [activePlayer, setActivePlayer] = useState("X");
+  const [gameTurns, setGameTurns] = useState([]);
+  // const [activePlayer, setActivePlayer] = useState("X");
 
-  function handleSelectSquare() {
-    setActivePlayer((curActivePlayer) => curActivePlayer === "X" ? "O" : "X");
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    setGameTurns((prevTurns) => {
+      // let currentPlayer = "X";
+
+      // if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+      //   currentPlayer = "O";
+      // }
+      const currentPlayer = deriveActivePlayer(prevTurns);
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex, player: currentPlayer } },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
-    <main id="game-container">
-      <ol id="players" className="highlight-player">
-        <Player initialName="player 1" symbol="X" isActive={activePlayer === "X"} />
-        <Player initialName="player 2" symbol="O" isActive={activePlayer === "O"} />
-      </ol>
-      <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
+        </ol>
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          // activePlayerSymbol={activePlayer}
+          turns={gameTurns}
+        />
+      </div>
+      <Log turns={gameTurns} />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
 export default function player({ initialName, symbol, isActive }) {
-  const [ playerName, setPlayerName ] = useState(initialName)
-  const [ isEditing, setisEditing ] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setisEditing] = useState(false);
 
   function handleEditClick() {
     //setisEditing(!isEditing); // 해당값이 반전 됨. 이 방법은 여러 번의 상태 업데이트가 일어날 때 정확하게 동작하지 않을 수 있음
@@ -18,7 +18,9 @@ export default function player({ initialName, symbol, isActive }) {
   // let btnCaption = "수정";
 
   if (isEditing) {
-    editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
     // btnCaption = "저장";
   }
 
@@ -28,7 +30,7 @@ export default function player({ initialName, symbol, isActive }) {
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>{ isEditing ? "저장" : "수정" }</button>
+      <button onClick={handleEditClick}>{isEditing ? "저장" : "수정"}</button>
     </li>
-  )
+  );
 }
